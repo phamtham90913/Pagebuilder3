20e086c8d9d040017b39bbc94363ec8b8c083106
thampham90913 <nguyenpham90913@gmail.com>
2018-09-10 19:01:21 +0700

    update code drag & drop

--- src/main/java/Common/WebDriverAction.java
+++ src/main/java/Common/WebDriverAction.java
@@ -1,18 +1,31 @@
 package Common;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.CacheLookup;
+
+import org.openqa.selenium.support.FindBy;
+
+import org.openqa.selenium.support.PageFactory;
 
 public class WebDriverAction {
 	public WebDriver dr;
 	public WebDriverAction(WebDriver driver) {
 		this.dr=driver;
+		  PageFactory.initElements(driver, this);
 	}
 	public WebDriverAction() {
 
 	}
-			
+	public WebDriverAction check(String name) {
+		return this;
+	}
 	public By getElement(String locator) {
 		By by=null;
 		if(locator.startsWith("id=")) {
@@ -25,7 +38,7 @@ public class WebDriverAction {
 			}else
 				if(locator.startsWith("linktext=")) {
 					locator=locator.substring(9);
-							by=By.linkText(locator);
+					by=By.linkText(locator);
 				}
 		return by;
 
@@ -40,44 +53,62 @@ public class WebDriverAction {
 	public void hover(String locator) {
 		Actions act = new Actions(dr);
 		act.moveToElement(dr.findElement(getElement(locator))).build().perform();
-		
+
 	}
-	
+
 	public void draganddrop(String from, String to) {
 
 		Actions act = new Actions(dr);
 		act.dragAndDrop(dr.findElement(getElement(from)), dr.findElement(getElement(to))).build().perform();
 
 	}
-		/*public void dragandrop(String element, int x, int y) {
-	
-			Actions act = new Actions(dr);
-			System.out.println("step 2");
-			WebElement from = dr.findElement(getElement(element));
-			System.out.println("step 23");
-			act.dragAndDropBy(from, x, y).build().perform();
-		}
-		@Test
-		public void callDraganddrop() {
-			System.setProperty("chromedriver", "D://Sel//chromedriver.exe");
-			dr = new ChromeDriver();
-			dr.manage().window().maximize();
-			dr.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-			System.out.println("step 1");
-			dr.get("http://demo.guru99.com/test/drag_drop.html");
-			dr.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-			System.out.println("step 1");
-			Actions act = new Actions(dr);
-			System.out.println("step 2");
-			WebElement from = dr.findElement(By.xpath("//li/a[contains(text(),'BANK')]"));
-			System.out.println("step 23");
-			WebElement to = dr.findElement(By.xpath("//*[@id='bank']/li"));
-			System.out.println("get name of Bank field:\t"+to.getText());
-			System.out.println("step 24");
-			act.dragAndDrop(from, to).build().perform();
-			dr.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-			System.out.println("get name of Bank field:\t"+to.getText());
-			assertEquals(dr.findElement(By.xpath("//*[@id='bank']/li\"")).getText(), "BANK");
+	public void dragandrop(String element, int x, int y) {
 
-		}*/
+		Actions act = new Actions(dr);
+		System.out.println("step 2");
+		WebElement from = dr.findElement(getElement(element));
+		System.out.println("step 23");
+		act.dragAndDropBy(from, x, y).release().perform();
+	}
+	public void dragbyOffset(String locator, int x, int y) throws InterruptedException {
+		/*System.setProperty("chromedriver.exe", "D://Sel//chromedriver.exe");
+		dr = new ChromeDriver();
+		dr.manage().window().maximize();
+		dr.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+		dr.get("http://demo.guru99.com/test/drag_drop.html");		
+		dr.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);*/
+		WebElement from = dr.findElement(getElement(locator));
+		Actions builder = new Actions(dr);
+//		try {
+			builder.clickAndHold(from).build().perform();
+			System.out.println("locator of from:\t"+from.getLocation().toString());
+			Thread.sleep(1000);
+			builder.moveByOffset(x, y).perform();
+			Thread.sleep(2000);
+			builder.release().perform();
+//		}
+//		catch(Exception e) {
+//			System.out.println(e);
+//		}
+	}
+	public void dragtoElement(String locatorFrom, String locatorTo) throws InterruptedException {
+		/*System.setProperty("chromedriver.exe", "D://Sel//chromedriver.exe");
+		dr = new ChromeDriver();
+		dr.manage().window().maximize();
+		dr.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+		dr.get("http://demo.guru99.com/test/drag_drop.html");		
+		dr.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);*/
+		WebElement from = dr.findElement(getElement(locatorFrom)),
+				to = dr.findElement(getElement(locatorTo));
+		Actions builder = new Actions(dr);
+		dr.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+			builder.clickAndHold(from).build().perform();
+			System.out.println("locator of from:\t"+from.getLocation().toString());
+			Thread.sleep(1000);
+			/*builder.moveByOffset(x, y).perform();*/			
+			builder.moveToElement(to).perform();
+			Thread.sleep(2000);
+			builder.release().perform();
+	}
+
 }
--- src/test/java/Common/AddArticle.java
+++ src/test/java/Common/AddArticle.java
@@ -4,6 +4,12 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+//import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
@@ -14,58 +20,108 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+
 public class AddArticle {
 	WebDriver dr;
-  @Test
-  public void f() {
-	  this.dr=Common.dr;
-	  WebDriverAction myact = new WebDriverAction(dr);
-	  System.out.println("pass 1");
-	  dr.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-	  myact.click("xpath=//ul[@id='menu']//li/a[text()='Content ']");
-	  System.out.println("pass 2");
-	  myact.hover("xpath=//li/a[@class='dropdown-toggle menu-article']/..");
-	  myact.click("linktext=Add New Article");
-	  dr.manage().timeouts().implicitlyWait(35, TimeUnit.SECONDS);
-	  myact.click("xpath=//a[@id='ui-show-catalog-button']/../a[2]");
-	  dr.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-	  myact.click("xpath=//div[text()='Paragraph']");
-	  dr.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-	  myact.draganddrop("xpath=//div[@class='sc-jDwBTQ fkdWmV']/img[3]", "xpath=//div[@data-type='Body']");
-	  System.out.println("pass all");
-  }
-  @BeforeMethod
-  public void beforeMethod() {
-  }
+	@Test
+	public void createArticle() throws InterruptedException {
+		this.dr=Common.dr;
+		WebDriverAction myact = new WebDriverAction(dr);
+		System.out.println("pass 1");
 
-  @AfterMethod
-  public void afterMethod() {
-  }
+		dr.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		myact.click("xpath=//ul[@id='menu']//li/a[text()='Content ']");
+		System.out.println("pass 2");
+		myact.hover("xpath=//li/a[@class='dropdown-toggle menu-article']/..");
+		myact.click("linktext=Add New Article");
+		dr.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+	/*	myact.click("xpath=//a[@id='ui-show-catalog-button']/../a[2]");
+		dr.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		myact.click("xpath=//div[text()='Paragraph']");
+		dr.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		String location1 = dr.findElement(By.xpath("//img[@name='Paragraph']/../img[1]")).getLocation().toString();
+		System.out.println("locaiton of image:\t"+location1);
+		//		wait for element visible
+		WebDriverWait wait = new WebDriverWait(dr, 10);		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(myact.getElement("xpath=//img[@name='Paragraph']/../img[1]")));
+		wait.until(ExpectedConditions.elementToBeClickable(myact.getElement("xpath=//img[@name='Paragraph']/../img[1]")));
+		myact.dragbyOffset("xpath=//img[@name='Paragraph']/../img[1]", -500, 225);
+		//	  myact.dragtoElement("xpath=//img[@name='Paragraph']/../img[1]","xpath=//div[@data-type='Body']" );
+		String location = dr.findElement(By.xpath("//img[@name='Paragraph']/../img[1]")).getLocation().toString();
+		System.out.println("locaiton of image:\t"+location);
+		System.out.println("pass all");*/
+		dr.findElement(myact.getElement("xpath=//div[@data-type='Body']")).click();
+		Thread.sleep(2000);
+		String getText = dr.findElement(myact.getElement("xpath=//div[@data-type='Body']/div/div/h3")).getText();
+		
+		System.out.println("placeholder text is:\t"+getText);
+	}
+	@BeforeMethod
+	public void beforeMethod() {
+	}
 
-  @BeforeClass
-  public void beforeClass() {
-	  Common cm = new Common(dr);
-	  cm.login("http://bravebits.io/j5", "admin", "1");
-  }
+	@AfterMethod
+	public void afterMethod() {
+	}
 
-  @AfterClass
-  public void afterClass() {
-  }
+	@BeforeClass
+	public void beforeClass() {
+		Common cm = new Common(dr);
+		cm.login("http://bravebits.io/j5", "admin", "1");
+	}
 
-  @BeforeTest
-  public void beforeTest() {
-  }
+	@AfterClass
+	public void afterClass() {
+	}
 
-  @AfterTest
-  public void afterTest() {
-  }
+	@BeforeTest
+	public void beforeTest() {
+	}
 
-  @BeforeSuite
-  public void beforeSuite() {
-  }
+	@AfterTest
+	public void afterTest() {
+	}
 
-  @AfterSuite
-  public void afterSuite() {
-  }
+	@BeforeSuite
+	public void beforeSuite() {
+	}
+
+	@AfterSuite
+	public void afterSuite() {
+	}
+
+	public void drag() {
+		System.setProperty("chromedriver.exe", "D://Sel//chromedriver.exe");
+		dr = new ChromeDriver();
+		dr.manage().window().maximize();
+		dr.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+		dr.get("http://demo.guru99.com/test/drag_drop.html");		
+		dr.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+		//		WebDriverAction myact = new WebDriverAction(dr);
+		//		myact.dragandrop("xpath=//li[@id='credit2']/a", 350, 613);
+		WebElement from = dr.findElement(By.xpath("//li[@id='credit2']/a"));
+		Actions builder = new Actions(dr);
+		try {
+			dr.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+			builder.clickAndHold(from).moveByOffset(350, 613).release().build();
+
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+
+	}
+	// @Test
+	public void dragtooffset() throws InterruptedException {
+		WebDriverAction myact = new WebDriverAction();
+		myact.dragbyOffset("xpath=//li[@id='credit2']/a",-308, 226);
+
+	}
+	//  @Test
+	public void dragtoelement() throws InterruptedException {
+		WebDriverAction myact = new WebDriverAction();
+		myact.dragtoElement("xpath=//li[@id='credit2']/a","xpath=//ol[@id='bank']");
+
+	}
 
 }
